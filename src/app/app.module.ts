fe58b82469f18120796dbac5503b27389f8f655c
leelaplap <letandat27101998@gmail.com>
2019-11-19 08:47:07 +0700

    progress bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProgessBarComponent } from './progess-bar/progess-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgessBarComponent
   ],
   imports: [
     BrowserModule
